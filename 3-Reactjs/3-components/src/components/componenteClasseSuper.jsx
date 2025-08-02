bd2ed6421e4d665c53469a9b7749e5505d492ae4
William José <williamjmrosa@gmail.com>
2025-08-02 09:17:56 -0300

    modificou

--- 3-Reactjs/3-components/src/components/componenteClasseSuper.jsx
+++ 3-Reactjs/3-components/src/components/componenteClasseSuper.jsx
@@ -1,22 +1,18 @@
-import { Component } from "react"
+import { Component } from 'react'
 
 class ComponenteClasseSuper extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            nome: this.props.nome
+            nome: this.props.nome 
         }
     }
 
     render() {
-        return
-            <h1>Olá, {this.state.nome}</h1>
+        return <h1>Olá, {this.state.nome}</h1>
     }
-
 }
 
-function App(){
-    return <ComponenteClasseSuper nome="William" />
+function App() {
+    return <ComponenteClasseSuper nome = 'Taís' />
 }
-
-

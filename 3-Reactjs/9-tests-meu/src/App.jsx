50cd7a3d03a8785503b471c363ea4e8db244cced
William José <williamjmrosa@gmail.com>
2025-08-20 19:40:56 -0300

    atualizado

--- 3-Reactjs/9-tests-meu/src/App.jsx
+++ 3-Reactjs/9-tests-meu/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from "react"
 import CounterJSX from "./components/CounterJSX"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
       <div>
         <h1>Counter com JSX</h1>
         <CounterJSX />
       </div>
-    </>
   )
 }
 

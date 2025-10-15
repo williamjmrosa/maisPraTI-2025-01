6b36af13fce3097906783ea354611c2b8b806ec2
William José <williamjmrosa@gmail.com>
2025-10-15 20:10:18 -0300

    Meu

---
+++ 4-Java/api-example-meu/src/main/java/com/t1/api_example/course/Course.java
@@ -0,0 +1,21 @@
+//package com.t1.api_example.course;
+//
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.validation.constraints.NotBlank;
+//import jakarta.validation.constraints.Size;
+//
+//public class Course {
+//
+//  @Id
+//  @GeneratedValue(strategy = GenerationType.IDENTITY)
+//  private Long id;
+//
+//  @NotBlank
+//  @Size(max =150)
+//  private String name;
+//
+//
+//
+//}
